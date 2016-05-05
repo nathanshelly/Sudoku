dccab6bc08b0ae9b398c89f3b57a438b993e9b3a
Nathan Shelly <nathanshelly2018@u.northwestern.edu>
2016-05-04 21:36:40 -0500

    Getting up to date

--- Test.py
+++ Test.py
@@ -1,13 +1,10 @@
 from SudokuStarter import *
 
-tempBoard = init_board('input_puzzles/easy/4_4.sudoku')
+tempBoard = init_board('input_puzzles/easy/16_16.sudoku')
 
 tempBoard.print_board()
 
 winBoard = solve(tempBoard)
 winBoard.print_board()
 
-# print openSpots(tempBoard)
-# print(tempBoard.getSubSquareDomain(2, 0))
-# print(tempBoard.getRowDomain(2, 0))
-# print(tempBoard.getColDomain(2,0))
+print is_complete(winBoard)
