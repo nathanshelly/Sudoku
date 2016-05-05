e260ce73eda81c334ee98c198fb1bd0906c2c567
Nathan Shelly <nathanshelly2018@u.northwestern.edu>
2016-05-04 22:06:43 -0500

    Edited Test.py

--- Test.py
+++ Test.py
@@ -1,10 +1,16 @@
 from SudokuStarter import *
 
 tempBoard = init_board('input_puzzles/easy/16_16.sudoku')
+file = open("results.txt", "w")
 
 tempBoard.print_board()
+
 
 winBoard = solve(tempBoard)
 winBoard.print_board()
 
+
 print is_complete(winBoard)
+
+file.write(str(is_complete(winBoard)))
+file.close()
